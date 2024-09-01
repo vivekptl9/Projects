de1d16948c41eee8158536765186a9c854c70ecb
vivekptl9 <vivekptl9@gmail.com>
2024-09-01 20:15:30 +0200

    Using cloud scraping tool to scrape the website

--- AIWebScrap/main.py
+++ AIWebScrap/main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from scrape import scrape_website   
+from scrape import scrape_website, extract_body_content,split_dom_content,clean_body_content
 
 st.title("AI Web Scraper")
 url = st.text_input("Enter a Website URL: ")
@@ -8,33 +8,17 @@ url = st.text_input("Enter a Website URL: ")
 if st.button("Scrape Site"):
     st.write("Scraping the website")
     result = scrape_website(url)
-    print(result)
 
 
-from selenium.webdriver import Remote, ChromeOptions
-from selenium.webdriver.chromium.remote_connection import ChromiumRemoteConnection
+    body_content =extract_body_content(result)
+    clean_content=clean_body_content(body_content)
 
-SBR_WEBDRIVER = 'https://brd-customer-hl_e04092c2-zone-ai_scraper:5rrp3xvzhnrs@brd.superproxy.io:9515'
-
-
-def main():
-    print('Connecting to Scraping Browser...')
-    sbr_connection = ChromiumRemoteConnection(SBR_WEBDRIVER, 'goog', 'chrome')
-    with Remote(sbr_connection, options=ChromeOptions()) as driver:
-        print('Connected! Navigating to https://example.com...')
-        driver.get('https://example.com')
-        # CAPTCHA handling: If you're expecting a CAPTCHA on the target page, use the following code snippet to check the status of Scraping Browser's automatic CAPTCHA solver
-        print('Waiting captcha to solve...')
-        solve_res = driver.execute('executeCdpCommand', {
-            'cmd': 'Captcha.waitForSolve',
-            'params': {'detectTimeout': 10000},
-        })
-        print('Captcha solve status:', solve_res['value']['status'])
-        print('Navigated! Scraping page content...')
-        html = driver.page_source
-        print(html)
+    st.session_state.dom_content = clean_content
+    with st.expander("View DOM Content"):
+        st.text_area("Dom Content", clean_content, height=300)
 
 
-if __name__ == '__main__':
-    main()
+    
+
+
     
