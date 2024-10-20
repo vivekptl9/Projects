01f7dfda81830cf4f8c81b515819089a62f00f65
vivekptl9 <vivekptl9@gmail.com>
2024-10-20 17:11:27 +0200

    MLOPS
    Former-commit-id: 86b81b5f95a09b54e81e0e09448e2c51ef737921

--- MLOPS/Flask/app.py
+++ MLOPS/Flask/app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route("/")
 def welcome():
-    return "Welcome to the Flask Tutorial"
+    return "<html><H1>Welcome to the Flask Tutorial</H1></html>"
+@app.route("/index")
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
