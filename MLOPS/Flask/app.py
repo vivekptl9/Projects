fd1ab134ead8fdf783ed14c4b1d3769e5acf7e68
vivekptl9 <vivekptl9@gmail.com>
2024-10-22 12:46:38 +0200

    Dynamic sites using flask
    Former-commit-id: 5bf3d1eb4640c26bc8c7339345182f87aa48fdf8

--- MLOPS/Flask/app.py
+++ MLOPS/Flask/app.py
@@ -1,12 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 @app.route("/")
 def welcome():
     return "<html><H1>Welcome to the Flask Tutorial</H1></html>"
-@app.route("/index")
+
+@app.route("/index",methods=['GET'])
 def index():
     return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/form",methods=['GET','POST'])
+def form():
+    if request.method == 'POST':
+        name = request.form['name']
+        return f'Hello {name}!'
+    else:
+        return render_template('form.html')
+    
+@app.route("/submit",methods=['GET','POST'])
+def submit():
+    if request.method == 'POST':
+        name = request.form['name']
+        return f'Hello {name}!'
+    else:
+        return render_template('form.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
